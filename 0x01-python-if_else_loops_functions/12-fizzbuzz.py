e421f079b7bd4bea6ef508b362fc3a6e46a87780
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-03-08 02:57:10 -0800

    prints the numbers from 1 to 100 separated by a space.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,10 @@
 def fissbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FissBuzz", end='')
+            print("FissBuzz", end=' ')
         elif i % 3 == 0:
-            print("Fiss", end='')
+            print("Fiss", end=' ')
         elif i % 5 == 0:
-            print("Buzz", end='')
+            print("Buzz", end=' ')
         else:
-            print(i, end='')
-        print(" ", end='')
+            print(i, end=' ')
