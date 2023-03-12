0c7b47c51d7d18d022c760266fd587ada09cfb65
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-03-12 05:20:26 -0700

    checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -17,5 +17,6 @@ struct listint_s *next;
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
+int check_pal(listint_t **head, listint_t *last);
 int is_palindrome(listint_t **head);
 #endif /* LISTS_H */
