c0abc788773c1fe089ff760ba17c78552a319f41
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-04-04 08:01:37 -0700

    class Rectangle that defines a rectangle by: (based on 2-rectangle.py)

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+"""
+A module made up of a rectangle class
+with two instance variable width and
+height.It has two public instance
+methods
+"""
+
+
+class Rectangle:
+    """
+    rectangle class
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        returned the setter value
+        """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        set the width value
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        returned the setter value
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        set the height value
+        """
+
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        returned the rectangle area
+        """
+
+        return (self.width * self.height)
+
+    def perimeter(self):
+        """
+        returned the perimeter
+        of the rectangle
+        """
+
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (2 * (self.width + self.height))
+
+    def __str__(self):
+        """
+        returned rectangle
+        printed in #
+        """
+
+        strg = ''
+        if self._width == 0 or self._height == 0:
+            return strg
+        else:
+            for m in range(self.width):
+                for n in range(self.height):
+                    strg += '#'
+                strg += '\n'
+            return strg[0:-1]
