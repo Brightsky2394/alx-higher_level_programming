8c36737b2769d51c9f51dd20de56b92d55661555
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-04-04 10:22:29 -0700

    class Rectangle that defines a rectangle by: (based on 8-rectangle.py)

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -122,6 +122,7 @@ class Rectangle:
         elif rect_1.area() >= rect_2.area():
             return rect_1
 
+    @classmethod
     def square(cls, size=0):
         """
         returned new Rectangle instance
