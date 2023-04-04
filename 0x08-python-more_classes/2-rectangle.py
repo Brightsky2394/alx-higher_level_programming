ebb8ac002f1a268761aa661010cee4f382aa8e4b
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-04-04 06:58:55 -0700

    class Rectangle that defines a rectangle by: (based on 1-rectangle.py)

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
         set the width value
         """
 
-        if type(value) not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
