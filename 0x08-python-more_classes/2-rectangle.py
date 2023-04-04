4fd4f2370d4eb00bf0daa4952f61fcbced154c7d
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-04-04 06:20:25 -0700

    class Rectangle that defines a rectangle by: (based on 1-rectangle.py)

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+A module made up of a rectangle class
+with two instance variable width and
+height.It has two public instance
+methods
+"""
+
+
+class Rectangle:
+    """
+    rectangle class
+    """
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        returned the setter value
+        """
+
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        set the width value
+        """
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width
+
+    @property
+    def height(self):
+        """
+        returned the setter value
+        """
+
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        set the height value
+        """
+
+        if isinstance(value, int) is False:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        returned the rectangle area
+        """
+
+        return (self.width * self.height)
+
+    def perimeter(self):
+        """
+        returned the perimeter
+        of the rectangle
+        """
+
+        return (2 * (self.width * self.height))
