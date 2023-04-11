16fbe176a4b0befed1dbaa2a6bb1e5476bd58858
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-04-11 12:13:05 -0700

    class Square that inherits from Rectangle (9-rectangle.py). (task based on 10-square.py)

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+BaseGeometry class with
+public instance method
+raising an exception
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    square class a
+    subclass to rectangle
+    class
+    """
+
+    def __init__(self, size):
+        """
+        instantiate the size parameter
+        """
+        super.integer_validator("size", size)
+        self.size = size
+        super.__init__(size, size)
+
+    def area(self):
+        """
+        compute and return val
+        of area
+        """
+
+        val = self.__size ** 2
+        return val
+
+    def __str__(self):
+        """
+        return an object in a
+        more readable form
+        """
+
+        cnt = "[{}] " + "{}" + "/" + "{}"
+        return cnt.format(self.__class__.__name__, self.__size, self.__size)
