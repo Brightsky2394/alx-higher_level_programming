426022da888fb94df3f7ba1e71f99d83c2645f64
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-06-13 09:10:10 -0700

    prints the first argument passed to it

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args[2] === null)
-{
-	console.log("No argument");
-}
-else{
-	console.log(args[2]);
+if (args[2] === null) {
+  console.log('No argument');
+} else {
+  console.log(args[2]);
 }
