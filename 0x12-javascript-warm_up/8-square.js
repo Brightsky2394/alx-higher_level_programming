e37dd46c2e16ff6aeb1a1888a0ac44ea93eeaf95
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-06-13 10:50:04 -0700

    prints a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
 const args = process.argv;
 const value = parseInt(args[2]);
-if (isNaN(value))
-{
-	console.log("Missing size");
-}
-else
-{
-	let i;
-	for (i = 0; i < value; i++)
-	{
-		console.log("X".repeat(value));
-	}
+if (isNaN(value)) {
+  console.log('Missing size');
+} else {
+  let i;
+  for (i = 0; i < value; i++) {
+    console.log('X'.repeat(value));
+  }
 }
