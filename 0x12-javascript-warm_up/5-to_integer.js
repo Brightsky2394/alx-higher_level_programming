64a5e507e5f5e183e57faca5b18a259fdb478587
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-06-13 09:52:04 -0700

    prints My number: <first argument converted in integer>

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 const args = process.argv;
 const value = parseInt(args[2]);
-if (isNaN(value))
-{
-	let res = "Not a number";
-	console.log(res);
-}
-else
-{
-	let res1 = "My number: " + value;
-	console.log(res1);
+if (isNaN(value)) {
+  const res = 'Not a number';
+  console.log(res);
+} else {
+  const res1 = 'My number: ' + value;
+  console.log(res1);
 }
