afc58a366f1be87a6a0835e881582e4fa6162500
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-06-13 10:30:21 -0700

    prints 3 lines texts using an array of string and a loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const langCnt = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (const j of langCnt) {
-  console.log(j);
+const len = langCnt.length - 1;
+for (let j = 0; j <= len; j++) {
+  console.log(langCnt[j]);
 }
