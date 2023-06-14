206ff280a815fc5ea2f7ea5c6c63672019d630d9
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-06-14 09:57:28 -0700

    Create an instance method called charPrint(c)

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
-const square5 = require('./5-square');
-class Square extends square5 {
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
   charPrint (c) {
     if (typeof (c) === 'undefined') {
       c = 'X';
-    } else {
-      let i = 0;
-      while (i < this.height) {
-        console.log(c.repeat(this.width));
-        i++;
-      }
+    }
+    let i = 0;
+    while (i < this.height) {
+      console.log(c.repeat(this.width));
+      i++;
     }
   }
 }
