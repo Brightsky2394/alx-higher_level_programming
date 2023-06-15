1e80281238a9aacd4294f5859e1a0efef1b28015
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-06-15 01:59:16 -0700

    concatenate two files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-let content = '';
-content = content.concat(fs.readFileSync(process.argv[2]));
-content = content.concat(fs.readFileSync(process.argv[3]));
-fs.writeFileSync(process.argv[4], content);
+const f1 = fs.readFileSync(process.argv[2], 'utf8');
+const f2 = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], f1 + f2);
