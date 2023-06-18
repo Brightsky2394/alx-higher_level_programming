5af3713c595cbd294c5772f9d89307b98831b308
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-06-18 08:25:22 -0700

    lists all states with a name starting with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
-"""
-lists all states with a name starting with N
-"""
+""" lists all states with a name starting with N """
 
-import MySQLdb
 from sys import argv
+import MySQLdb
 
-if __name__ == '__main__':
-    """
-    Access to the database and get the states
-    from the database.
-    """
-    connection = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                                 passwd=argv[2], db=argv[3])
-
-    curr_obj = db.cursor()
-    curr_obj.execute("SELECT * FROM states \
-                      WHERE name LIKE BINARY 'N%' \
-                      ORDER BY states.id ASC")
-    rows = curr_obj.fetchall()
-
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+    connection = MySQLdb.connect(host="localhost",
+                                 port=3306,
+                                 user=username,
+                                 passwd=password,
+                                 db=db_name)
+    curr = connection.cursor()
+    curr.execute("SELECT states.id, name FROM states WHERE name "
+                 "COLLATE latin1_general_cs "
+                 "LIKE 'N%' "
+                 "ORDER BY states.id ASC;")
+    rows = curr.fetchall()
     for row in rows:
         print(row)
-curr_obj.close()
-connection.close()
+
+    curr.close()
+    connection.close()
