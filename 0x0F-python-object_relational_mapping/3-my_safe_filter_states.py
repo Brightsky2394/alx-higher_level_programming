0cb2427704cc2c3a92d81bbcf9bb5e780c64dc1f
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-06-19 05:26:44 -0700

    takes in arguments and displays all values in the states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-""" Prevent SQL Injection """
-
-from sys import argv
+""" import and print database MySQL """
 import MySQLdb
-
+from sys import argv
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    db_name = argv[3]
-    state_name = argv[4]
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=username,
-                           passwd=password,
-                           db=db_name)
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        password=argv[2],
+        db=argv[3]
+    )
     curr = conn.cursor()
-
-    query = """
-    SELECT states.id, name FROM states WHERE name = %s
-    COLLATE latin1_general_cs
-    ORDER BY states.id ASC;
-    """
-
-    curr.execute(query, (state_name, ))
-
-    rows = curr.fetchall()
-    for row in rows:
-        print(row)
-
+    curr.execute(
+        "SELECT * FROM states where name=%s ORDER BY states.id", (argv[4], )
+        )
+    for i in cur.fetchall():
+        print(i)
     curr.close()
     conn.close()
