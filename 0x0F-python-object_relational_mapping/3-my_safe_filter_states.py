81f7ffc31a81a918dd762426f386016c3d2c0b89
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-06-19 03:18:12 -0700

    takes in arguments and displays all values in the states table

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,33 +1,13 @@
 #!/usr/bin/python3
 """
-script that takes in arguments and
-displays all values in the states table.
-It's safe from MySQL injection!
+write a script that is free from SQL injections
 """
-
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    db_name = argv[3]
-    state_name = argv[4]
-    conn = MySQLdb.connect(host="localhost",
-                           port=3306,
-                           user=username,
-                           passwd=password,
-                           db=db_name)
+    conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     curr = conn.cursor()
-
-    query = """
-    SELECT states.id, name FROM states WHERE name = %s
-    COLLATE latin1_general_cs
-    ORDER BY states.id ASC;
-    """
-
-    curr.execute(query, (state_name, ))
-
-    rows = curr.fetchall()
-    for row in rows:
-        print(row)
+    curr.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                 (argv[4],))
+    [print(state) for state in curr.fetchall()]
