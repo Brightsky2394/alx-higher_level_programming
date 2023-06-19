55b61c8389dbb495566334b3f6d86c7ec87a1652
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-06-19 06:28:28 -0700

    lists all cities from the database hbtn_0e_4_usa

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
 """
-This script lists all cities from
-the database hbtn_0e_4_usa.
+lists all cities from the database hbtn_0e_4_usa
 """
 
-import MySQLdb
 from sys import argv
+import MySQLdb
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    username = argv[1]
+    password = argv[2]
+    db_name = argv[3]
+
+    conn = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=username,
+                           passwd=password,
+                           db=db_name)
+    curr_obj = conn.cursor()
+
+    query = """
+    SELECT cities.id, cities.name, states.name
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC;
     """
-    Access to the database and get the cities
-    from the database.
-    """
 
-    conn = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                           passwd=argv[2], db=argv[3])
+    curr_obj.execute(query)
 
-    with conn.cursor() as curr:
-        curr.execute("""
-            SELECT
-                cities.id, cities.name, states.name
-            FROM
-                cities
-            JOIN
-                states
-            ON
-                cities.state_id = states.id
-            ORDER BY
-                cities.id ASC
-        """)
+    rows = curr_obj.fetchall()
+    for row in rows:
+        print(row)
 
-        rows = curr.fetchall()
-
-    if rows is not None:
-        for row in rows:
-            print(row)
+    curr_obj.close()
+    conn.close()
