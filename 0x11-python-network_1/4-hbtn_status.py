bc57bfdf62c01328cafa38cceeb956274bde92e4
Sikiru Yaya <sikiruyaya1@gmail.com>
2023-07-02 10:02:33 -0700

    fetches https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://intranet.hbtn.io/status
-with `requests` module
-"""
+""" script that fetches https://alx-intranet.hbtn.io/status """
+from requests import get
 
-import requests
-
-
-if __name__ == "__main__":
-    req = requests.get('https://intranet.hbtn.io/status')
-
+if __name__ == '__main__':
+    res = get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type: {_type}'.format(_type=type(req.text)))
-    print('\t- content: {_content}'.format(_content=req.text))
+    print('\t- type:', type(res.text))
+    print('\t- content:', res.text)
