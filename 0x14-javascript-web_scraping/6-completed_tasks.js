e77ed70f5e4ab7d5c9632d42363fe2d718f333e9
skycode <sikiruyaya1@gmail.com>
2023-07-25 11:06:41 -0700

    computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,19 +1,21 @@
 #!/usr/bin/node
-const args = process.argv;
-const url = args[2];
+const argv = process.argv;
+const url = argv[2];
 const request = require('request');
-request(url, (err, res, body) => {
-  if (!err) {
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
     const resBody = JSON.parse(body);
     const resDict = {};
-    for (const j of resBody) {
-      if (j.completed === true) {
-        if (resDict[j.userId] === undefined) { resDict[j.userId] = 0; }
-        resDict[j.userId] += 1;
+    for (const i of resBody) {
+      if (i.completed === true) {
+        if (resDict[i.userId] === undefined) {
+          resDict[i.userId] = 0;
+        }
+        resDict[i.userId] += 1;
       }
     }
     console.log(resDict);
-  } else {
-    console.log('Error:', err);
   }
 });
